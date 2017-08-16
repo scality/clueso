da70b261b2fd535de492f44e59bd2545b0ea2890
arod <alex@arodrigues.com>
2017-08-17 00:33:02 +0100

    adds utils to read application.conf, apply conf to an existing spark session

--- src/main/scala/com/scality/clueso/SparkUtils.scala
+++ src/main/scala/com/scality/clueso/SparkUtils.scala
@@ -1,12 +1,22 @@
 package com.scality.clueso
 
+import java.io.File
 import java.net.URI
 
+import com.scality.clueso.query.MetadataQuery
+import com.typesafe.config.ConfigFactory
 import org.apache.hadoop.conf.{Configuration => HadoopConfig}
 import org.apache.hadoop.fs.{FileSystem, Path, PathFilter}
 import org.apache.spark.sql.SparkSession
 
 object SparkUtils {
+  def loadCluesoConfig(confFilePath: String) = {
+    val parsedConfig = ConfigFactory.parseFile(new File(confFilePath))
+    val _config = ConfigFactory.load(parsedConfig)
+
+    new CluesoConfig(_config)
+  }
+
   def buildHadoopFs(config: CluesoConfig) = {
     FileSystem.get(new URI(config.mergePath), hadoopConfig(config))
   }
@@ -30,6 +40,24 @@ object SparkUtils {
       .config("spark.hadoop.fs.s3a.access.key", config.s3AccessKey)
       .config("spark.hadoop.fs.s3a.secret.key", config.s3SecretKey)
       .config("spark.hadoop.fs.s3a.path.style.access", config.s3PathStyleAccess)
+  }
+
+  def confSparkSession(spark : SparkSession, config : CluesoConfig) = {
+    spark.conf.set("spark.hadoop.fs.s3a.connection.ssl.enabled", config.s3SslEnabled)
+    spark.conf.set("spark.hadoop.fs.s3a.endpoint", config.s3Endpoint)
+    spark.conf.set("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+    spark.conf.set("spark.hadoop.fs.s3a.access.key", config.s3AccessKey)
+    spark.conf.set("spark.hadoop.fs.s3a.secret.key", config.s3SecretKey)
+    spark.conf.set("spark.hadoop.fs.s3a.path.style.access", config.s3PathStyleAccess)
+  }
+
+  def getQueryResults(spark : SparkSession, query : MetadataQuery) = {
+//    import scala.util.parsing.json.JSONObject
+//
+//    val results = ()
+//
+//    results
+    query.execute.toJSON.collect
   }
 
   val parquetFilesFilter = new PathFilter {
