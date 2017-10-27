0c58aaa239171b0bcdd7171d4e0719a7194140df
arod <alex@arodrigues.com>
2017-10-27 09:56:10 +0100

    adds S3 submodule to build s3 docker images (required by integration tests)

---
+++ docker/images/alluxio/bin/run-s3-setup-alluxio-master.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#
+echo "Sleeping for 10 seconds to give time to S3 to come up"
+sleep 10;
+
+echo "Creating METADATA bucket in S3 (ENDPOINT = $ALLUXIO_UNDERFS_S3_ENDPOINT )"
+aws s3api create-bucket --bucket METADATA --endpoint-url $ALLUXIO_UNDERFS_S3_ENDPOINT
+aws s3api put-object --bucket METADATA --key alluxio/.ignore --endpoint-url $ALLUXIO_UNDERFS_S3_ENDPOINT
+aws s3api put-object --bucket METADATA --key alluxio/landing/.ignore --endpoint-url $ALLUXIO_UNDERFS_S3_ENDPOINT
+aws s3api put-object --bucket METADATA --key alluxio/staging/.ignore --endpoint-url $ALLUXIO_UNDERFS_S3_ENDPOINT
+
+/entrypoint.sh master
