ff9aa0d95260b2c2cabc832923d1015e77dce4b5
Lauren Spiegel <lhspiegel@gmail.com>
2017-11-09 17:56:42 -0800

    notes from meeting

--- docker/images/clueso-docker-livy/entrypoint.sh
+++ docker/images/clueso-docker-livy/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# use simpler way to get ip address
+# try just setting to 0.0.0.0
 if [ -z ${HOST+x} ]; then 
 # if host isn't known then check outbound IP when contacting google
   export LIBPROCESS_IP=$(ip route get 8.8.8.8 | head -1 | cut -d' ' -f8)
