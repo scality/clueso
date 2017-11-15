87c38c290bc9ca92f758c9f83803383db75c7091
arod <alex@arodrigues.com>
2017-11-15 19:28:34 +0000

    changes default query page size

--- src/main/scala/com/scality/clueso/query/MetadataQuery.scala
+++ src/main/scala/com/scality/clueso/query/MetadataQuery.scala
@@ -1,5 +1,5 @@
 package com.scality.clueso.query
 
-case class MetadataQuery(bucketName : String, sqlWhereExpr : String, start_key : Option[String], limit : Int = 100) {
+case class MetadataQuery(bucketName : String, sqlWhereExpr : String, start_key : Option[String], limit : Int = 1000) {
   override def toString: String = s"[MetadataQuery bucket=${bucketName}, query=$sqlWhereExpr, start_key=$start_key, end=$limit]"
 }
