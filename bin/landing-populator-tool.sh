d75cce46657e5176e5710e3d54dd674b41ca345a
arod <alex@arodrigues.com>
2017-11-16 17:01:58 +0000

    adds logic to tool scripts to be run from any dir

--- bin/landing-populator-tool.sh
+++ bin/landing-populator-tool.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-java -cp ../lib/clueso-tool.jar com.scality.clueso.tools.LandingMetadataPopulatorTool $*
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+java -cp $DIR/../lib/clueso-tool.jar com.scality.clueso.tools.LandingMetadataPopulatorTool $*
--- bin/table-compactor.sh
+++ bin/table-compactor.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-java -cp ../lib/clueso-tool.jar -Dspark.ui.port=4057 com.scality.clueso.tools.MetadataTableCompactorTool $* 2>&1 | grep -z "Compactor"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+java -cp $DIR/../lib/clueso-tool.jar -Dspark.ui.port=4057 com.scality.clueso.tools.MetadataTableCompactorTool $* 2>&1 | grep -z "Compactor"
