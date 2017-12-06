4c43879d15bb17101fc211ef96c0734acefe7c6e
Lauren Spiegel <lhspiegel@gmail.com>
2017-12-05 18:25:05 -0800

    TEST: LandingPop Test cleans up

--- src/test/scala/com/scality/clueso/LandingPopulatorSpec.scala
+++ src/test/scala/com/scality/clueso/LandingPopulatorSpec.scala
@@ -9,14 +9,12 @@ import org.scalatest.{Matchers, WordSpec}
 
 class LandingPopulatorSpec extends WordSpec with Matchers with SparkContextSetup {
   "Landing Populator" should {
-    "Scenario 1: populate landing" in {
+    "Scenario 1: populate landing" in withSparkContext {
+      (spark, config) =>
       val bucketName = "foobucket"
       val numberParquetFiles = "10"
       val configPath = getClass.getResource("/application.conf").toString.substring(5)
       LandingMetadataPopulatorTool.main(Array(configPath, bucketName, "100", numberParquetFiles))
-      val parsedConfig = ConfigFactory.parseFile(new File(getClass.getResource("/application.conf").getFile))
-      val _config = ConfigFactory.load(parsedConfig)
-      val config = new CluesoConfig(_config)
       val fs = SparkUtils.buildHadoopFs(config)
       fs.listStatus(new Path(config.landingPathUri, s"bucket=$bucketName/maxOpIndex=$numberParquetFiles"),
         SparkUtils.parquetFilesFilter).length shouldEqual numberParquetFiles.toInt
